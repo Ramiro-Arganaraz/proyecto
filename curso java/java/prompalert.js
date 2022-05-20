e22bf16c51ee38e450ebb09f1097dc947e79c407
Ramiro Arganaraz <rammaexe@gmail.com>
2022-05-20 19:49:57 -0300

    Información de lo trabajado

---
+++ curso java/java/prompalert.js
@@ -0,0 +1,11 @@
+let numero1=prompt("ingrese un numero");
+const numero2=5;
+let resultado=numero1+++numero2;
+alert(resultado);
+console.log(resultado);
+/*let nombre=prompt("ingresa el nombre");
+alert(nombre);
+console.log(nombre);
+let apellido=prompt("ingresa tu apellido");
+let estado=prompt("situacion laboral");
+alert(apellido +" situaion laboral "+ estado)*/
